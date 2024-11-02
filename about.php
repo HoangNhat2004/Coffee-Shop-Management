e2722c09a6e4d91042a736f45bf7e6a4663de149
HoangNhat2004 <nhnhat202@gmail.com>
2024-11-02 15:06:22 +0700

    Upload file

---
+++ about.php
@@ -0,0 +1,308 @@
+<!-- <?php
+
+      include 'components/connect.php';
+
+      session_start();
+
+      if (isset($_SESSION['user_id'])) {
+         $user_id = $_SESSION['user_id'];
+      } else {
+         $user_id = '';
+      };
+
+      ?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+   <?php include 'components/user_header.php'; ?>
+   <!-- header section ends -->
+
+   <div class="heading">
+      <h3>about us</h3>
+      <p><a href="home.php">Home</a> <span> / About</span></p>
+   </div>
+
+   <!-- Our Ower -->
+   <section class="card">
+
+      <img src="images/ksi.jpg" class="card-img" alt="...">
+
+      <div class="doc">
+         <h3 class="title">Our Owner</h3>
+         <br>
+         <p> <b>Nguyễn Hoàng Nhật</b></p>
+         <p> Manager, <br> Department of Cafe Cong Doan
+            <br>
+            <b>Office: </b> B406-B<br> <b> Email: </b> 522H0123@student.tdtu.edu.vn
+         </p>
+         <br>
+         <br>
+         <a href="https://www.facebook.com/hoangnhat.nguyen.353/" target="_blank" class="btn">Learn More</a>
+      </div>
+
+   </section>
+
+   <!-- Our Team -->
+
+
+   <section class="team">
+
+      <h1 class="title">Our Team</h1>
+
+      <div class="swiper-wrapper" style="padding-left: 210px;">
+
+         <div class="box">
+            <img src="images/kai.jpg" alt="">
+
+            <h2>Nguyễn Thanh Sang</h2>
+            <h3>StudentID: 522H0141</h3>
+         </div>
+
+         <div class="box">
+            <img src="images/speed.jpg" alt="">
+            <h2>Lê Đức Trung</h2>
+            <h3>StudentID: 522H0110</h3>
+         </div>
+
+         <div class="box">
+            <img src="images/fanum.jpg" alt="">
+            <h2>Võ Sỹ Thái Sơn</h2>
+            <h3>StudentID: 522H0169</h3>
+         </div>
+      </div>
+
+   </section>
+
+   <!-- about section starts  -->
+
+   <section class="about">
+
+      <div class="row">
+
+         <div class="image">
+            <img src="images/about-card.jpg" alt="">
+         </div>
+
+         <div class="content">
+            <h3>Our Mission</h3>
+            <p>At coffee shop our mission is to spread positive energy.</p>
+            <br>
+            <br>
+            <p> For over two decades, our family has used coffee as a catalyst for inspiring community, relationships, and adventures. We dedicate ourselves to the quality of our work and elevating coffee experiences. </p>
+            <a href="menu.html" class="btn">our menu</a>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- about section ends -->
+
+   <!-- steps section starts  -->
+
+   <section class="steps">
+
+      <h1 class="title">simple steps</h1>
+
+      <div class="box-container">
+
+         <div class="box">
+            <img src="images/step-1.png" alt="">
+            <h3>choose order</h3>
+            <p>NGƯỢC LÊN TÂY BẮC GÓI VỊ MỘC VỀ XUÔI.</p>
+         </div>
+
+         <div class="box">
+            <img src="images/step-2.png" alt="">
+            <h3>fast delivery</h3>
+            <p>NHANH NHƯ CÁCH NGƯỜI YÊU CŨ TRỞ MẶT.</p>
+         </div>
+
+         <div class="box">
+            <img src="images/step-3.png" alt="">
+            <h3>enjoy food</h3>
+            <p>VỊ NGON TRÊN TỪNG HÀM RĂNG.</p>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- steps section ends -->
+
+   <!-- reviews section starts  -->
+
+   <section class="reviews">
+
+      <h1 class="title">customer's reivews</h1>
+
+      <div class="swiper reviews-slider">
+
+         <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-1.png" alt="">
+               <p>Chúc quán 8386 mãi đỉnh mãi đỉnh</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>jon tactay</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-2.png" alt="">
+               <p>Cà phê ngon mà hơi rẻ cần bán đắt hơn</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>john cinema</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-3.png" alt="">
+               <p>Sẽ quay lại</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>jonny beef</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-4.png" alt="">
+               <p>Jonny cảm thấy thích thích</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>jonny Dang</h3>  
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-5.png" alt="">
+               <p>Rất thích không gian của quán</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>lil john</h3>
+            </div>
+
+            <div class="swiper-slide slide">
+               <img src="images/pic-6.png" alt="">
+               <p>10 điểm</p>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+               <h3>johny Joestar</h3>
+            </div>
+
+         </div>
+
+         <div class="swiper-pagination"></div>
+
+      </div>
+
+   </section>
+
+   <!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- footer section starts  -->
+   <?php include 'components/footer.php'; ?>
+   <!-- footer section ends -->=
+
+
+
+
+
+
+   <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+   <script>
+      var swiper = new Swiper(".reviews-slider", {
+         loop: true,
+         grabCursor: true,
+         spaceBetween: 20,
+         pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+         },
+         breakpoints: {
+            0: {
+               slidesPerView: 1,
+            },
+            700: {
+               slidesPerView: 2,
+            },
+            1024: {
+               slidesPerView: 3,
+            },
+         },
+      });
+   </script>
+
+</body>
+
+</html>
